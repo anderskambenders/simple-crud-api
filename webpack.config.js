776c8de5406f02235566d215305abcebc7293b12
anderskambenders <a.zetsmanis@mail.ru>
2025-05-09 23:30:48 +0300

    feat: add base structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index_bundle.cjs',
+    path: resolve(__dirname, 'dist'),
+  }
+};
